368b3d62fc6b1ecb2af94edfbfd40cdeada19fb9
Sebastiano0 <71315537+Sebastiano0@users.noreply.github.com>
2022-11-13 19:11:30 +0100

    notifica, 2 schermate scelta e countdown

--- timer/lib/main.dart
+++ timer/lib/main.dart
@@ -4,7 +4,12 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:numberpicker/numberpicker.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'noti.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+      
 void main() {
   runApp(const MyApp());
 }
@@ -32,25 +37,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
+
   var _currentValueMinutes = 0;
   var _currentValueSeconds = 1;
   var _currentValueHours = 0;
+
+  String remainingTime = "";
+  int initialCount = 0;
   double progress = 1.0;
   dynamic sub;
   bool paused = true;
   dynamic controller;
+  bool numberPickerVisibility = true;
+  bool stopped = false;
+
   String path = "assets/audio/audio.mp3";
   final AudioPlayer player = AudioPlayer();
   var bytes; //load audio from assets
-/*
-    - E`richiesta la gestione della subscription degli stream.
-    - Lo studente realizzerà un opprotuno StreamController che, come un oscillatore, 
-      genererà i tick (eventi di trigger).
-    - Lo stream di tick servirà per gestire, possibilmente trasformandolo in un altro stream di numeri, 
-      l'avanzamento del conto alla rovescia.
-*/
+
+  createStringRemainingTime() {
+    remainingTime = "";
+    if (_currentValueHours < 10) {
+      remainingTime += "0$_currentValueHours:";
+    } else {
+      remainingTime += "$_currentValueHours:";
+    }
+    if (_currentValueMinutes < 10) {
+      remainingTime += "0$_currentValueMinutes:";
+    } else {
+      remainingTime += "$_currentValueMinutes:";
+    }
+    if (_currentValueSeconds < 10) {
+      remainingTime += "0$_currentValueSeconds";
+    } else {
+      remainingTime += "$_currentValueSeconds";
+    }
+  }
 
   play() async {
+    numberPickerVisibility = false;
     controller = StreamController<int>(
       onPause: () => print('Paused'),
       onResume: () => print('Resumed'),
@@ -62,61 +88,70 @@ class _MyHomePageState extends State<MyHomePage> {
         (_currentValueMinutes * 60) +
         _currentValueSeconds;
 
-    sub = controller.stream.listen(
-        (int data) async {
-          setState(() {
-            progress = ((progress) -
-                    1 /
-                        ((_currentValueHours * 24) +
-                            (_currentValueMinutes * 60) +
-                            _currentValueSeconds))
-                .toDouble();
-            if (_currentValueSeconds != 0) {
+    var fractionProgress = 1 / count / 10;
+    sub = controller.stream.listen((int data) async {
+      setState(() {
+        progress = progress - fractionProgress;
+        createStringRemainingTime();
+        if (data % 10 == 0) {
+          if (_currentValueSeconds != 0) {
+            if (data != 0) {
               _currentValueSeconds--;
+            }
+          } else {
+            _currentValueSeconds = 59;
+            if (_currentValueMinutes != 0) {
+              _currentValueMinutes--;
             } else {
-              _currentValueSeconds = 59;
-              if (_currentValueMinutes != 0) {
-                _currentValueMinutes--;
-              } else {
-                if (_currentValueHours != 0) {
-                  _currentValueMinutes = 59;
-                  _currentValueHours--;
-                }
+              if (_currentValueHours != 0) {
+                _currentValueMinutes = 59;
+                _currentValueHours--;
               }
             }
-          });
-        },
-        onError: (error) {},
-        onDone: () {
-          print("object");
-        });
+          }
+        }
+      });
+    }, onError: (error) {}, onDone: () {});
 
     if (!controller.isClosed) {
-      final stream = Stream.periodic(const Duration(seconds: 1), (x) {
+      final stream = Stream.periodic(const Duration(milliseconds: 100), (x) {
         return x;
-      }).take(count);
+      }).take(count * 10);
 
       await controller.addStream(stream);
 
-      if (_currentValueSeconds == 0) {
-        bytes = await rootBundle.load(path); //load audio from assets
-        Uint8List audiobytes =
-            bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
-        await player.playBytes(audiobytes);
-      }
+      if (progress == 0) {}
     }
+    _currentValueSeconds--;
+    initialCount = 0;
     sub.cancel();
     paused = true;
+    numberPickerVisibility = true;
+    Noti.initialize(flutterLocalNotificationsPlugin);
+
+    if (!stopped) {
+      bytes = await rootBundle.load(path); //load audio from assets
+      Uint8List audiobytes =
+          bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+      await player.playBytes(audiobytes);
+    } else {
+      stopped = false;
+    }
   }
 
   finish() {}
 
   pause() {
+    Noti.showBigTextNotification(
+        title: "Timer app", body: "Your timer has been paused", fln: flutterLocalNotificationsPlugin);
     sub.pause();
   }
 
   stop() {
+    stopped = true;
+    numberPickerVisibility = true;
     sub.cancel();
+    initialCount = 0;
     setState(() {
       _currentValueHours = 0;
       _currentValueMinutes = 0;
@@ -128,105 +163,128 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          SizedBox(
-            width: 300,
-            height: 300,
-            child: CircularProgressIndicator(
-              backgroundColor: Colors.grey.shade300,
-              value: progress,
-              strokeWidth: 6,
-            ),
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            NumberPicker(
-              minValue: 0,
-              maxValue: 24,
-              value: _currentValueHours,
-              onChanged: (value) => setState(() => _currentValueHours = value),
-              haptics: true,
-              infiniteLoop: true,
-              textStyle: const TextStyle(fontSize: 15, color: Colors.white),
-              selectedTextStyle:
-                  const TextStyle(fontSize: 30, color: Colors.white),
-            ),
-            const Text(
-              ":",
-              style: TextStyle(fontSize: 40, color: Colors.white),
-            ),
-            NumberPicker(
-              minValue: 0,
-              maxValue: 59,
-              value: _currentValueMinutes,
-              onChanged: (value) =>
-                  setState(() => _currentValueMinutes = value),
-              haptics: true,
-              infiniteLoop: true,
-              textStyle: const TextStyle(fontSize: 15, color: Colors.white),
-              selectedTextStyle:
-                  const TextStyle(fontSize: 30, color: Colors.white),
-            ),
-            const Text(
-              ":",
-              style: TextStyle(fontSize: 40, color: Colors.white),
-            ),
-            NumberPicker(
-              minValue: 0,
-              maxValue: 59,
-              value: _currentValueSeconds,
-              onChanged: (value1) => setState(() {
-                _currentValueSeconds = value1;
-              }),
-              haptics: true,
-              infiniteLoop: true,
-              textStyle: const TextStyle(fontSize: 15, color: Colors.white),
-              selectedTextStyle:
-                  const TextStyle(fontSize: 30, color: Colors.white),
-            ),
-          ]),
+          Visibility(
+              visible: !numberPickerVisibility,
+              child: SizedBox(
+                width: 300,
+                height: 300,
+                child: Stack(children: <Widget>[
+                  Center(
+                    child: SizedBox(
+                      width: 300,
+                      height: 300,
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.grey.shade900,
+                        value: progress,
+                        strokeWidth: 4,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text(remainingTime,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 30)),
+                  )
+                ]),
+              )),
+          Visibility(
+              visible: numberPickerVisibility,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                NumberPicker(
+                  minValue: 0,
+                  maxValue: 23,
+                  value: _currentValueHours,
+                  onChanged: (value) =>
+                      setState(() => _currentValueHours = value),
+                  haptics: true,
+                  infiniteLoop: true,
+                  textStyle: const TextStyle(fontSize: 15, color: Colors.white),
+                  selectedTextStyle:
+                      const TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                const Text(
+                  ":",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
+                ),
+                NumberPicker(
+                  minValue: 0,
+                  maxValue: 59,
+                  value: _currentValueMinutes,
+                  onChanged: (value) =>
+                      setState(() => _currentValueMinutes = value),
+                  haptics: true,
+                  infiniteLoop: true,
+                  textStyle: const TextStyle(fontSize: 15, color: Colors.white),
+                  selectedTextStyle:
+                      const TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                const Text(
+                  ":",
+                  style: TextStyle(fontSize: 40, color: Colors.white),
+                ),
+                NumberPicker(
+                  minValue: 0,
+                  maxValue: 59,
+                  value: _currentValueSeconds,
+                  onChanged: (value1) => setState(() {
+                    _currentValueSeconds = value1;
+                  }),
+                  haptics: true,
+                  infiniteLoop: true,
+                  textStyle: const TextStyle(fontSize: 15, color: Colors.white),
+                  selectedTextStyle:
+                      const TextStyle(fontSize: 30, color: Colors.white),
+                ),
+              ])),
           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            // IconButton(
-            //     onPressed: paused
-            //         ? () {
-            //             setState(() {
-            //               paused = false;
-            //             });
-            //           }
-            //         : null,
-            //     icon: const Icon(Icons.play_arrow,
-            //         size: 50, color: Colors.white)),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    paused = true;
-                  });
-                  stop();
-                },
-                icon: const Icon(
-                  Icons.stop,
-                  size: 50,
-                  color: Colors.white,
-                )),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    if (paused) {
-                      paused = false;
-                      play();
-                    } else {
-                      paused = true;
-                      pause();
-                    }
-                  });
-                },
-                icon: Icon(paused == false ? Icons.pause : Icons.play_arrow,
-                    size: 50, color: Colors.white)),
+            Visibility(
+                visible: !numberPickerVisibility,
+                child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        paused = true;
+                      });
+                      stop();
+                    },
+                    icon: const Icon(
+                      Icons.stop,
+                      size: 50,
+                      color: Colors.white,
+                    ))),
+            Visibility(
+                visible: numberPickerVisibility,
+                child: IconButton(
+                    onPressed: paused
+                        ? () {
+                            setState(() {
+                              paused = false;
+                              play();
+                            });
+                          }
+                        : null,
+                    icon: const Icon(Icons.play_arrow,
+                        size: 50, color: Colors.white))),
+            Visibility(
+                visible: !numberPickerVisibility,
+                child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (paused) {
+                          paused = false;
+                          play();
+                        } else {
+                          paused = true;
+                          pause();
+                        }
+                      });
+                    },
+                    icon: Icon(paused == false ? Icons.pause : Icons.play_arrow,
+                        size: 50, color: Colors.white))),
           ])
         ],
       )),
--- timer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ timer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers
+import flutter_local_notifications
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
