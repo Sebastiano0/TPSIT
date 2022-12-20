583e8ba612af12257d081b4306d19676df7ead08
Sebastiano0 <71315537+Sebastiano0@users.noreply.github.com>
2022-12-20 22:14:30 +0100

    client e server

---
+++ chatroom/client testuale/client.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+late Socket socket;
+
+void main() {
+  Socket.connect("localhost", 3000).then((Socket sock) {
+    socket = sock;  
+    socket.listen(dataHandler,
+        onError: errorHandler, onDone: doneHandler, cancelOnError: false);
+  }, onError: (e) {
+    print("Unable to connect: $e");
+    exit(1);
+  });
+
+  // connect standard in to the socket
+  stdin
+      .listen((data) => socket.write(String.fromCharCodes(data).trim() + '\n'));
+}
+
+void dataHandler(data) {
+  print(String.fromCharCodes(data).trim());
+}
+
+void errorHandler(error, StackTrace trace) {
+  print(error);
+}
+
+void doneHandler() {
+  socket.destroy();
+  exit(0);
+}
